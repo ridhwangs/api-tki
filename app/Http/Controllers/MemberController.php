1c97c7d762a0b591f88dcb943cb99c1e217ffdcf
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2021-11-26 19:05:09 +0700

    fixing expired date if member free

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -49,10 +49,10 @@ class MemberController extends Controller
                         ->first();
         if(!empty($query)){
             if($query->jenis_member == 'free'){
-                $expired_date = date('Y-m-d', strtotime($registrasi_date->addDays(30)));
+                $expired_date = null;
                 $days = 0;
             }else{
-                 $registrasi_date = Carbon::createFromFormat('Y-m-d', $query->tgl_awal);
+                $registrasi_date = Carbon::createFromFormat('Y-m-d', $query->tgl_awal);
                 $daysToAdd = $query->jumlah_hari;
                 $expired_date = date('Y-m-d', strtotime($registrasi_date->addDays($daysToAdd)));
 
