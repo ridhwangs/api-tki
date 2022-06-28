7d284e236d1791261c36e0e39065ac6ca89597d3
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2022-06-29 01:57:55 +0700

    add model

---
+++ app/Models/Card.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    protected $table = 'card_table';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
+}
