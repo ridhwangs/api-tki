3ad1c71161978e40cb8d721022e671c7053c9cec
Muhammad Ridhwan Ginanjar Sutresna <ridhwangs@gmail.com>
2022-06-29 07:27:24 +0700

    add database configuration multiple

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -1,12 +1,19 @@
 <?php
 
+
 namespace App\Models;
 
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Card extends Model
 {
+
+    use HasFactory;
+    protected $connection = 'db_hunian';
     protected $table = 'card_table';
+    public $timestamps = false;
     /**
      * The attributes that are mass assignable.
      *
